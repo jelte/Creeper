3421aabb6ae9f2ef850bb303abaecdb76fe0dedd
Jelte Steijaert <jelte.steijaert@gmail.com>
2018-01-26 20:31:09 +0000

    Add basic Player Controller

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	[Range(1.0f, 10.0f)]
+	public float speed = 5f;
+	[Range(0.1f, 2.0f)]
+	public float jumpModifier = 0.5f;
+	[Range(0.1f, 1.0f)]
+	public float climbModifier = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+	Character character;
+
+	// Use this for initialization
+	void Start () {
+		character = GetComponent<Character> (); 
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Initialize movement
+		Vector2 movement = Vector2.zero;
+		// Add horizontal movement
+		movement.x = Input.GetAxis ("Horizontal") * character.speed;
+
+		// Add vertical velocity for jump
+		if (Input.GetButtonDown ("Jump") && rb2d.velocity.y == 0) {
+			rb2d.velocity += Physics2D.gravity * -1 * character.jumpModifier;
+		}
+
+		// Add movement to position based on deltaTime
+		rb2d.position += movement * Time.deltaTime;
+	}
+}
