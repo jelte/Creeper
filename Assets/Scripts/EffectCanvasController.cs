0ec811fa830b650f64a168fc69762f613d74b78b
Chaowen <bailixionline@hotmail.com>
2018-01-27 16:48:01 +0000

    Add 3 effects UI and Canva

---
+++ Assets/Scripts/EffectCanvasController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EffectCanvasController : MonoBehaviour {
+
+    public bool isSwitchedLeftandRight;
+    public GameObject LR;
+    public bool isGravityUp;
+    public GameObject GU;
+    public bool isGravityDown;
+    public GameObject GD;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LR.SetActive(isSwitchedLeftandRight);
+        GU.SetActive(isGravityUp);
+        GD.SetActive(isGravityDown);		
+	}
+}
