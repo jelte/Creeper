b7bd994b05c96e7ec7681bb2fd7383facb24326f
Jelte Steijaert <jelte.steijaert@gmail.com>
2018-01-27 22:02:49 +0000

    Move pause menu to seperate scene

---
+++ Assets/Scripts/Pauzer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pauzer : MonoBehaviour {
+
+	int pauseSceneBuildIndex;
+
+	void Start() {
+		pauseSceneBuildIndex = SceneUtility.GetBuildIndexByScenePath ("Scenes/PauseScene");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Pause")) {
+			if (Time.timeScale > 0f) {
+				Time.timeScale = 0f;
+				SceneManager.LoadSceneAsync (pauseSceneBuildIndex, LoadSceneMode.Additive);
+			} else {
+				SceneManager.GetSceneByBuildIndex (pauseSceneBuildIndex).GetRootGameObjects () [0].GetComponentInChildren<Animator> ().SetTrigger ("hide");
+				StartCoroutine (Continue ());
+			}
+		}
+	}
+
+	IEnumerator Continue()
+	{
+		yield return new WaitForSecondsRealtime(0.2f);
+		Time.timeScale = 1f;
+		SceneManager.UnloadSceneAsync (pauseSceneBuildIndex);
+	}
+}
