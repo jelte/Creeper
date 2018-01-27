d6ab9572a7fd0f348213994f9d12509c69763633
Poon <poom1995@gmail.com>
2018-01-27 16:53:00 +0000

    update

---
+++ Assets/Scripts/MovingSpikeTrap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSpikeTrap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player"))
+        {
+            //reduce player health
+            collision.GetComponent<Character>().PlayerHealth = -1;
+
+            Debug.Log("Player took damage");
+
+        }
+    }
+
+}
