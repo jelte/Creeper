60c418951a3e2123773e06f785b77e2453615d21
Chaowen <bailixionline@hotmail.com>
2018-01-27 18:42:01 +0000

    add  UI_pause_panel
    Esc to trigger it

--- Assets/Scripts/EffectCanvasController.cs
+++ Assets/Scripts/EffectCanvasController.cs
@@ -9,14 +9,45 @@ public class EffectCanvasController : MonoBehaviour {
     public GameObject GU;
     public GameObject GD;
 
+    public GameObject puasePanel;
+    bool isPausePanelShow;
+    Animator ani;
+
 	private TraitManager traitManager;
 
 	// Use this for initialization
 	void Start () {
+        isPausePanelShow = false;
+        ani = puasePanel.GetComponent<Animator>();
 	}
-	
+
+    IEnumerator switchPauseState(bool showOrNot, float waitTime)
+    {
+        
+        Time.timeScale = 1;
+        isPausePanelShow = showOrNot;
+        yield return new WaitForSeconds(waitTime);        
+        if (showOrNot)
+            Time.timeScale =  0;  
+        
+    }
+
 	// Update is called once per frame
 	void Update () {
+
+        // trigger Pause panel
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (isPausePanelShow)
+                StartCoroutine(switchPauseState(false, 0.36f));
+            else
+            {                
+                StartCoroutine(switchPauseState(true, 0.36f));
+            }
+        }
+
+        Debug.Log(isPausePanelShow);
+
 		if (traitManager == null && GameObject.FindGameObjectWithTag ("Player")) {
 			traitManager = GameObject.FindGameObjectWithTag ("Player").GetComponent<TraitManager> ();
 		}
@@ -26,4 +57,9 @@ public class EffectCanvasController : MonoBehaviour {
 			GD.SetActive (traitManager.isBouncy);
 		}
 	}
+
+    private void FixedUpdate()
+    {
+        ani.SetBool("show", isPausePanelShow);   
+    }
 }
