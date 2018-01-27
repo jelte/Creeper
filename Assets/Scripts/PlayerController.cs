62e66cf0840ff8c45c4aa7301eec63794d447752
Chaowen <bailixionline@hotmail.com>
2018-01-27 14:02:24 +0000

    Add attack and die animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,16 @@ using XInputDotNetPure; // Required in C#
 
 public class PlayerController : MonoBehaviour {
 
+    //for movement
 	Rigidbody2D rb2d;
 	Character character;
 
+    //for animation use
     Animator ani;
     float aniSpeed;
+    bool attackPressed = false;
+    bool canAttack = true;
+    bool faceRight = true;
     int jumping = 0;
 
     // Use this for initialization
@@ -39,23 +44,43 @@ public class PlayerController : MonoBehaviour {
 			rb2d.velocity += Physics2D.gravity * -1f * (character.jumpModifier/jumping);
 		}
 
-		// Trigger Attack
-		if (Input.GetButtonDown ("Fire1")) {
-			character.Attack ();
+        // Trigger Attack
+        if (aniSpeed > 0)
+            faceRight = true;
+        if (aniSpeed < 0)
+            faceRight = false;
+		if (Input.GetButtonDown ("Fire1")&&canAttack) {			
+            StartCoroutine(Attack(0.5f));
 		}
+        Debug.Log(attackPressed);
+    }
+
+    IEnumerator Attack(float waitTime)
+    {
+        
+        canAttack = false;
+        attackPressed = true;
+        character.Attack();
+        yield return new WaitForSeconds(waitTime); ;
+        attackPressed = false;
+        canAttack = true;
     }
 
     private void FixedUpdate()
     {
-  
+        
+        // ground check
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, LayerMask.GetMask("Ground"));
         if (hit.collider != null)
         {
             jumping = 0;
         }
 
+        // animation set
         ani.SetFloat("speed", aniSpeed);
         ani.SetFloat("velocity", rb2d.velocity.y);
         ani.SetBool("land", jumping > 0);
+        ani.SetBool("faceRight", faceRight);
+        ani.SetBool("attack", attackPressed);
     }
 }
