f5b7e8c36d5fc7d6cc49b03b62abb527ff16287c
Jelte Steijaert <jelte.steijaert@gmail.com>
2018-01-28 15:17:49 +0000

    Fix pause quit

--- Assets/Scripts/PauseMenuLoader.cs
+++ Assets/Scripts/PauseMenuLoader.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using XInputDotNetPure;
 
 public class PauseMenuLoader : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Quit")) 
+		if (Input.GetKeyDown(KeyCode.Return) || GamePad.GetState(0).Buttons.Back == ButtonState.Pressed) 
 		{
 			Application.Quit ();
 			Debug.Log ("I HAVE QUIT");
