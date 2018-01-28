feac02e7f589e3ae4639f761b108ac89fb69188f
Jelte Steijaert <jelte.steijaert@gmail.com>
2018-01-28 14:41:46 +0000

    Fix Pauzer

--- Assets/Scripts/Pauzer.cs
+++ Assets/Scripts/Pauzer.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class Pauzer : MonoBehaviour {
 
+	bool hiding = false;
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Pause") && !GameManager.CharacterDied) {
 			if (Time.timeScale > 0f) {
 				Time.timeScale = 0f;
 				SceneManager.LoadSceneAsync (SceneUtility.GetBuildIndexByScenePath ("Scenes/PauseScene"), LoadSceneMode.Additive);
-			} else {
+			} else if (!hiding) {
+				hiding = true;
 				SceneManager.GetSceneByBuildIndex (SceneUtility.GetBuildIndexByScenePath ("Scenes/PauseScene")).GetRootGameObjects () [0].GetComponentInChildren<Animator> ().SetTrigger ("hide");
 				StartCoroutine (Continue ());
 			}
@@ -23,5 +26,6 @@ public class Pauzer : MonoBehaviour {
 		yield return new WaitForSecondsRealtime(0.2f);
 		Time.timeScale = 1f;
 		SceneManager.UnloadSceneAsync (SceneUtility.GetBuildIndexByScenePath ("Scenes/PauseScene"));
+		hiding = false;
 	}
 }
