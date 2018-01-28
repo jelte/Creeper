d8208043df7af61003314074559c9ad2e776bfc7
PESMIT <patrickelliotsmith@hotmail.com>
2018-01-28 14:43:47 +0000

    PS - Pause Menu Update and XBOX Back Button

---
+++ Assets/PauseMenuLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Return) || Input.GetButtonDown("XBOX BACK")) 
+		{
+			Application.Quit ();
+			Debug.Log ("I HAVE QUIT");
+		}
+	}
+}
