9b9bb0d3d71923509bcf00264759e765b69a248d
Jelte Steijaert <jelte.steijaert@gmail.com>
2018-02-09 22:00:37 +0000

    Character & Character Controller revamp

---
+++ Assets/Scripts/Events/CharacterActionEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectFTP.Events
+{
+    public class CharacterActionEventArgs : EventArgs
+    {
+        public Character.Action action;
+
+        public CharacterActionEventArgs(Character.Action action)
+        {
+            this.action = action;
+        }
+    }
+}
