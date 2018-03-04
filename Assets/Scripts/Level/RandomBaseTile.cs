8ed3c6a1c2e41580dd0b3232605ef1d2bb3024de
Poon <poom1995@gmail.com>
2018-03-04 20:15:49 +0000

    added random base tiles, spinning spikes animation

---
+++ Assets/Scripts/Level/RandomBaseTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBaseTile : MonoBehaviour {
+
+
+    public Sprite[] SpriteArray;
+    public int RandomTile;
+
+    // Use this for initialization
+    void Start ()
+    {
+        RandomTile = Random.Range(1, 26);
+        this.GetComponent<SpriteRenderer>().sprite = SpriteArray[RandomTile];
+    }
+	
+}
