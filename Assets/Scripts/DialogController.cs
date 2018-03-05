2214436873a58e53169718030a295ece75f2f1d8
bailixi <bailixionline@hotmail.com>
2018-03-05 12:53:39 +0000

    Dialog add, change the virus to a hack pic, I am not sure when we need to show it so there is no script to trigger it yet. But there is a simple script to change showing text content.

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : MonoBehaviour {
+
+    public Text TextArea;
+
+    public int showingMessage;
+    public string[] message;
+	
+	// Update is called once per frame
+	void Update () {
+        if (message != null)
+            TextArea.text = message[showingMessage];
+    }
+}
