1bb45edec9b28fd226ecb50021d48995c32ee86e
bailixi <bailixionline@hotmail.com>
2018-03-06 23:29:29 +0000

    What changed: 1) seperated all things in dialog scene       - Dialog Fram (one defalut, one white)       - Dialog Background (defalut black)       - Hacker Background (defalut green)       - Hacker Fram (one defalut, one white)       - Name area (edit able)
    individual file can be find in Asserts > ArtAssets > UISpirits > UI_Diolog

--- Assets/Scripts/DialogController.cs
+++ Assets/Scripts/DialogController.cs
@@ -5,13 +5,19 @@ using UnityEngine.UI;
 
 public class DialogController : MonoBehaviour {
 
-    public Text TextArea;
+    public Text NameArea;
+    public int NameMessage;
+    public string[] Name;
 
+
+    public Text TextArea;
     public int showingMessage;
     public string[] message;
 	
 	// Update is called once per frame
 	void Update () {
+        if (Name != null)
+            NameArea.text = Name[NameMessage];
         if (message != null)
             TextArea.text = message[showingMessage];
     }
