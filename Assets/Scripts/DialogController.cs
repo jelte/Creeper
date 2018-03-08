e599e8a7505ea84021ea64a933b3d522ea4e53b7
Jelte Steijaert <jelte@users.noreply.github.com>
2018-03-08 00:14:24 +0000

    Dialog (#71)
    * Dialog add, change the virus to a hack pic, I am not sure when we need to show it so there is no script to trigger it yet. But there is a simple script to change showing text content.
    
    * What changed:
    1) seperated all things in dialog scene
          - Dialog Fram (one defalut, one white)
          - Dialog Background (defalut black)
          - Hacker Background (defalut green)
          - Hacker Fram (one defalut, one white)
          - Name area (edit able)
    
    individual file can be find in Asserts > ArtAssets > UISpirits > UI_Diolog

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : MonoBehaviour {
+
+    public Text NameArea;
+    public int NameMessage;
+    public string[] Name;
+
+
+    public Text TextArea;
+    public int showingMessage;
+    public string[] message;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Name != null)
+            NameArea.text = Name[NameMessage];
+        if (message != null)
+            TextArea.text = message[showingMessage];
+    }
+}
