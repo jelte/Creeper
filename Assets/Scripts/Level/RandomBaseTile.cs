e8e281c48406c202f570c7c617cb0dce4a8f640a
Jelte Steijaert <jelte@users.noreply.github.com>
2018-03-08 00:12:44 +0000

    world 0 (#70)
    * added random base tiles, spinning spikes animation
    
    * All levels overhaul. added 0-1, 0-2, 0-3, 0-4 for World 1 and 1-1,1-2 for World 2
    
    * edited level corruptions & tweaked Base tile spawn sprite

---
+++ Assets/Scripts/Level/RandomBaseTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBaseTile : MonoBehaviour {
+
+
+    public Sprite[] SpriteArray;
+    public int RandomTile;
+
+    // Use this for initialization
+    void Start ()
+    {
+        RandomTile = Random.Range(1, 34);
+        this.GetComponent<SpriteRenderer>().sprite = SpriteArray[RandomTile];
+    }
+	
+}
